de63599115ecd4f223d290fb9856d3ed9887593a
Andrew Schmidthuber <aschmidthuber@gmail.com>
2017-07-08 11:24:55 -0500

    Added basic level manager and level transitions for testing

---
+++ AwfulSummerJam2017/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+
+    static string currentLevelName; // will be used later to reload current level
+
+
+    // turn into singleton
+
+    void Awake()
+    {
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void LoadLevel(string levelName)
+    {
+
+        SceneManager.LoadSceneAsync(levelName);
+
+    }
+    public void LoadLevel(int sceneIndex)
+    {
+        SceneManager.LoadSceneAsync(sceneIndex);
+
+    }
+
+
+
+
+
+}
---
+++ AwfulSummerJam2017/Assets/Scripts/TransitionToMain.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransitionToMain : MonoBehaviour
+{
+    LevelManager levelManager;
+    public int mainMenuSceneIndex = 4;
+    // Use this for initialization
+    void Start()
+    {
+        levelManager = GameObject.FindObjectOfType<LevelManager>();
+
+        Invoke("LoadMainMenu", 2f);
+    }
+    void LoadMainMenu()
+    {
+        levelManager.LoadLevel(mainMenuSceneIndex);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
