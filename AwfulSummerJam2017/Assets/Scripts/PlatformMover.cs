f6a4416f318c70d8ab0a83a31139f9032297aad9
Lysdexique <acorbeil@gmail.com>
2017-07-09 15:48:58 -0400

    Prototype1
    +added JankyBoy.png as a prototype player sprite
    +added JankyBeev.png as a prototype of a moving obstacle
    +created "empty" platform prefab which only contains the ground4
    +created PlatformPool prefab
    +created Player Prefab
    +created Beaver prefab
    +added PlayerBehaviour script which checks the ground and lets the player jump (only once), also has a collision detection with obstacles
    +added PlatformMover script, which moves the platform left
    +added Shredder script and prefab, which destroys any object it touches
    +added EnemyBehaviour script, so far it only lets the enemy move left, will need to beef it up
    +Added Obstacles tag
    *modified Level 1 text location

---
+++ AwfulSummerJam2017/Assets/Scripts/PlatformMover.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformMover : MonoBehaviour
+{
+    public float speed = 10;
+
+    void Update()
+    {
+        transform.Translate(Vector3.left * speed * Time.deltaTime);
+    }
+	
+}
---
+++ AwfulSummerJam2017/Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    public float jumpForce;
+    public bool isGrounded; //public for testing purposes, change to private once done
+    public LayerMask whatIsGround;
+
+    private Rigidbody2D rb;
+    private Collider2D myCollider;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        myCollider = GetComponent<Collider2D>();
+    }
+
+    void Update()
+    {
+        isGrounded = Physics2D.IsTouchingLayers(myCollider, whatIsGround);
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            if(isGrounded)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            }
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        EnemyBehaviour enemy = col.gameObject.GetComponent<EnemyBehaviour>();
+
+        if(enemy)
+        {
+            Debug.Log("Hells bells! I appear to have been hit!");
+        }
+    }
+
+}
