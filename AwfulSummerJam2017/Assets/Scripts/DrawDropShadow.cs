29293c98892166068973976ee02af2d1eeeda1de
Lysdexique <acorbeil@gmail.com>
2017-07-14 18:57:32 -0400

    Merge branch 'master' of https://github.com/calicode/AwfulSummerJam2017

---
+++ AwfulSummerJam2017/Assets/Scripts/DrawDropShadow.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawDropShadow : MonoBehaviour
+{
+    float dropShadowPosition;
+    float shadowOffsetY;
+    Vector3 parentBounds;
+    SpriteRenderer spriteRender;
+    LayerMask groundLayer;
+
+    // Use this for initialization
+    void Start()
+    {
+        groundLayer = LayerMask.NameToLayer("Ground");
+
+        spriteRender = GetComponent<SpriteRenderer>();
+        UpdateShadowHeight();
+    }
+
+    public void UpdateShadowSize()
+    {
+        float scaleChange = (transform.parent.position.y + transform.position.y);
+        // this needs to be modified  so the scaling is less drastic
+
+        //Debug.Log(+scaleChange);
+        transform.localScale = new Vector2(scaleChange, scaleChange);
+
+    }
+
+    public void UpdateShadowHeight()
+    {
+        // change this to a raycast to closest solid layer
+
+        // call from playerbehavior after jump if grounded and ypos changed much
+        RaycastHit2D hit = Physics2D.Raycast(transform.parent.position, Vector2.down, 15, groundLayer);
+        if (hit) { Debug.Log("Raycast hit below at distance of" + hit.distance); }
+        parentBounds = transform.parent.GetComponent<Renderer>().bounds.extents;
+        shadowOffsetY = -(parentBounds.y * 2);
+
+    }
+
+
+    void Update()
+    {
+        UpdateShadowHeight();
+        UpdateShadowSize();
+
+        transform.position = new Vector2(transform.position.x, shadowOffsetY);
+    }
+}
