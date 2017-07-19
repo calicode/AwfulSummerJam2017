69c2162f5bfbdbb7d155ddb8d59f1c3ec18285e7
Andrew Schmidthuber <aschmidthuber@gmail.com>
2017-07-19 11:17:27 -0500

    Added Music manager and supporting set volume script. Put in a few songs for testing, not final. Will cut down song length once we have a better feel of level length

--- AwfulSummerJam2017/Assets/Scripts/MusicManager.cs
+++ AwfulSummerJam2017/Assets/Scripts/MusicManager.cs
@@ -1,16 +1,100 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class MusicManager : MonoBehaviour {
+public class MusicManager : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    static MusicManager instance = null;
+
+    public AudioClip[] musicArray;
+    AudioSource audioSource;
+    const string PLAYER_PREFS_VOLUME = "AWFULJAM_2017_BOOZE-BLUES_VOLUMEKEY";
+
+
+
+    // Use this for initialization
+    void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+            Debug.Log("Destroying extra music manager");
+        }
+
+
+        else
+        {
+            instance = this; DontDestroyOnLoad(gameObject);
+        }
+
+
+        audioSource = GetComponent<AudioSource>();
+        audioSource.volume = GetVolume();
+        PlayMusicTrack(SceneManager.GetActiveScene().buildIndex); // only needed for testing so main menu has a song. can remove in final build
+
+    }
+
+
+    public void OnLevelWasLoaded()
+    {
+        PlayMusicTrack(SceneManager.GetActiveScene().buildIndex);
+
+    }
+
+    public void PlayMusicTrack(int sceneIndex)
+    {
+
+        AudioClip clip = musicArray[sceneIndex];
+
+        if (clip)
+        {
+            audioSource.clip = clip;
+            audioSource.PlayDelayed(1f);
+            audioSource.loop = true;
+        }
+        else { Debug.Log("No clip in MusicManager's musicArray for this scene. Add clip or just ignore this"); }
+
+    }
+
+
+
+
+
+
+
+    // set by SetMusicVolume script which is on main menu canvas. 
+
+    public void SetVolume(float newVolume)
+    {
+
+        if (newVolume <= 1.0f)
+        {
+            PlayerPrefs.SetFloat(PLAYER_PREFS_VOLUME, newVolume);
+            audioSource.volume = GetVolume();
+        }
+        else { Debug.Log("Tried to set volume too high, this shouldn't appear"); }
+
+    }
+
+    public float GetVolume()
+    {
+        float prefsVolume = PlayerPrefs.GetFloat(PLAYER_PREFS_VOLUME);
+
+        if (prefsVolume <= 1.0f) { return prefsVolume; }
+        else
+        {
+            Debug.Log("Playerprefs volume somehow got set too high, sending back default volume");
+            SetVolume(1.0f);
+            return 1.0f;
+        }
+
+
+
+    }
+
+
+
+
 }
---
+++ AwfulSummerJam2017/Assets/Scripts/SetMusicVolume.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class SetMusicVolume : MonoBehaviour
+{
+
+    public Slider slider;
+    MusicManager musicManager;
+    // Use this for initialization
+    void Start()
+    {
+        musicManager = FindObjectOfType<MusicManager>();
+        slider.value = musicManager.GetVolume();
+    }
+
+    // Update is called once per frame
+
+
+
+    void Update()
+    {
+        musicManager.SetVolume(slider.value);
+    }
+}
