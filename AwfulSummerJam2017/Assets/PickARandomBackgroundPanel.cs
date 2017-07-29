2ff44b3004cfa944ab5f24a065c5333396e1e4d0
Andrew Schmidthuber <aschmidthuber@gmail.com>
2017-07-29 11:56:42 -0500

    Added random background panel code to level 2.

---
+++ AwfulSummerJam2017/Assets/PickARandomBackgroundPanel.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickARandomBackgroundPanel : MonoBehaviour
+{
+
+    /*This should be placed on the platform pool for a level. */
+
+
+    SpriteRenderer spriteRender;
+    public Sprite[] panelArray; // assign background panels from Sprites\BG folder
+
+
+
+
+    // Put this on platform pool for a level   
+
+
+    void Start()
+    {
+        foreach (Transform child in transform)
+        {
+            if (child.name.Contains("Platform"))
+            {
+                AssignBackground(child.Find("BG"));
+            }
+        }
+
+    }
+    void AssignBackground(Transform transform)
+    {
+        spriteRender = transform.GetComponent<SpriteRenderer>();
+        spriteRender.sprite = panelArray[Random.Range(0, panelArray.Length)];
+    }
+
+
+}
